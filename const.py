6714d5820501bd91a9b6470e7ebbf72a969ee62d
= <=>
2024-08-11 22:53:17 +0530

    add const.py

---
+++ const.py
@@ -0,0 +1,25 @@
+from enum import StrEnum
+from dataclasses import dataclass
+
+class DeviceType(StrEnum):
+    SWITCH = "onoff"
+    FAN = "dimmer"
+
+@dataclass
+class Device:
+    """API device."""
+    device_id: int
+    mac_id: str
+    room: str
+    device_unique_id: str
+    device_type: DeviceType
+    name: str
+    state: int | bool
+
+DOMAIN = "toyama"
+TOYAMA_API_HOST = "https://api.toyamaworld.com"
+
+PLATFORMS = [
+    "switch",
+    "fan"
+]
